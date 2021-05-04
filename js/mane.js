702e789d7f042237c9a6451bcbfef61b0b43c4f2
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-05-04 19:40:37 +0300

    1

--- js/mane.js
+++ js/mane.js
@@ -1,14 +1,18 @@
 import galleryList from './gallery-items.js';
 console.log(galleryList);
-
+// галерея картинок
 const galleryContainerRef = document.querySelector('.js-gallery');
+// модалка
 const modalContainerRef = document.querySelector('.js-lightbox');
+// картинка в модалке
 const modalImageRef = document.querySelector('.lightbox__image');
+// кнопка закрыть модалку
+const closeModalImageRef = document.querySelector('[data-action="close-lightbox"]');
+
 
 const cardsMarkup = createGalleryCardsMarkup(galleryList);
 
-galleryContainerRef.addEventListener('click', onGalleryClick);
-
+// создали разметку
 function createGalleryCardsMarkup(galleryList) {
   return galleryList
     .map(({ preview, original, description }) => {
@@ -34,7 +38,10 @@ function createGalleryCardsMarkup(galleryList) {
 galleryContainerRef.insertAdjacentHTML('beforeend', cardsMarkup);
 console.log(galleryContainerRef);
 
-function onGalleryClick(event) {
+galleryContainerRef.addEventListener('click', onGalleryContainerClick);
+
+
+function onGalleryContainerClick(event) {
 console.log(event.target.alt);
 
  const isGaleryImage = event.target.classList.contains('.gallery__image');
@@ -44,6 +51,12 @@ console.log(event.target.alt);
   event.preventDefault();
 
   modalContainerRef.classList.add('is-open');
-  modalImageRef.setAttribute('src', event.target.getAttribute('data-source'));
+  modalImageRef.setAttribute('src', event.target.getAtribute('${data-source}'));
   
+  closeModalImageRef.addEventListener ('click', onCloseModalImageClick);
+  
+}
+
+function onCloseModalImageClick () {
+  modalContainerRef.classList.remove('is-open');
 }
